67ca84689ca27414617138f072f00bc76de9d39f
DOHYUN JUNG <firegum74@gmail.com>
2025-07-02 15:09:19 +0900

    수정.

--- SSLServerApplication/DataHandler.cpp
+++ SSLServerApplication/DataHandler.cpp
@@ -21,9 +21,10 @@ void DataHandler::add_session(int session_id, std::shared_ptr<SSLSession> sessio
     auto it = session_buckets[shard].find(session_id);
     if (it != session_buckets[shard].end()) {
         std::cerr << "[add_session] FATAL: session_id " << session_id << " already exists! Closing previous session." << std::endl;
-        if (it->second) it->second->close_session();
+        if (it->second) it->second->close_session();  // 세션을 종료
         session_buckets[shard].erase(it);
     }
+	// 중복 검사 후 세션 추가
     session_buckets[shard][session_id] = session;
     std::cout << "[add_session] session_id=" << session_id << " added to shard " << shard << std::endl;
 }
